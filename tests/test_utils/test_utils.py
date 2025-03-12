5bf272331d554e335de56192cae02350e87d5665
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-03-12 11:37:33 +0100

    Add tests utils

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -2,7 +2,38 @@ import subprocess
 import os
 import shutil
 import json
+import docker
 from time import sleep
+
+test_data_examples = "test_data/example"
+test_apps = os.path.join(test_data_examples, "app")
+test_dockers = os.path.join(test_data_examples, "docker")
+test_packages = os.path.join(test_data_examples, "package")
+
+
+def does_image_exist(image: str) -> bool:
+    """Check if the image exists in the local Docker registry."""
+    client = docker.from_env()
+    try:
+        client.images.get(image)
+        return True
+    except docker.errors.ImageNotFound:
+        return False
+
+
+def get_available_images():
+    return sorted(os.listdir(test_dockers))
+
+
+def check_stdout(stdout: str, expected_result: bool):
+    stdout = stdout.lower()
+    if expected_result:
+        assert "build ok" in stdout
+        assert "error" not in stdout
+        assert "failed to" not in stdout
+    else:
+        assert "error" in stdout.upper()
+        assert "failed to build docker image:" in stdout
 
 
 def run_packager(
