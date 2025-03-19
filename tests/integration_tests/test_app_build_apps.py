4c5f500dc8a192866adb5eb3a42944d06592e2eb
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-03-19 10:57:48 +0100

    Add app tests

---
+++ tests/integration_tests/test_app_build_apps.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+from time import sleep
+
+from test_utils.test_utils import run_packager, does_image_exist, check_stdout, is_package_tracked, prepare_packages
+
+
+def test_01_build_app(test_image, packager_binary, context, test_repo):
+    """TODO"""
+    package = "test_package_1"
+    prepare_packages([package])
+
+    run_packager(
+        packager_binary,
+        "build-package",
+        context=context,
+        image_name=test_image,
+        output_dir=test_repo,
+        package_name=package,
+    )
