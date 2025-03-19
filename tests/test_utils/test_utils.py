16f2ec0e7c8c4d5a3414c20b524794e7bf00d286
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-03-19 10:28:05 +0100

    Add testing utils

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -9,12 +9,13 @@ from time import sleep
 
 test_config = {
     "test_data_examples": "test_data/example",
-    "test_apps": os.path.join("test_data/example", "app"),
-    "test_dockers": os.path.join("test_data/example", "docker"),
-    "test_packages": os.path.join("test_data/example", "package"),
+    "test_apps": os.path.abspath("test_data/example/app"),
+    "test_dockers": os.path.abspath("test_data/example/docker"),
+    "test_packages": os.path.abspath("test_data/example/package"),
     "packager_binary": os.path.abspath("../bap-builder/bap-builder"),
     "test_repo": os.path.abspath("test_data/test_repo"),
     "install_sysroot": os.path.abspath("install_sysroot"),
+    "test_packages_source": os.path.abspath("test_data/test_packages"),
 }
 test_config["test_images"] = sorted(os.listdir(test_config["test_dockers"]))
 
@@ -31,6 +32,10 @@ def init_test_repo():
 def clean():
     if os.path.exists(test_config["install_sysroot"]):
         shutil.rmtree(test_config["install_sysroot"])
+
+    if os.path.exists(test_config["test_packages"]):
+        shutil.rmtree(test_config["test_packages"])
+    os.makedirs(test_config["test_packages"])
     init_test_repo()
 
     pass
@@ -55,6 +60,14 @@ def check_stdout(stdout: str, expected_result: bool):
     else:
         assert "error" in stdout
         assert "failed to build" in stdout
+
+
+def prepare_packages(packages: list[str]):
+    for package in packages:
+        package_path = os.path.join(test_config["test_packages"], package)
+        if os.path.exists(package_path):
+            shutil.rmtree(package_path)
+        shutil.copytree(os.path.join(test_config["test_packages_source"], package), package_path)
 
 
 def is_package_tracked(package_name: str, repo_path: str) -> bool:
