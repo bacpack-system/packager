6b1c2b3475fe165b5f3e47921efce4a0aba0d714
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-03-26 14:09:08 +0100

    Add  sysroot tests

--- tests/integration_tests/test_create_sysroot.py
+++ tests/integration_tests/test_create_sysroot.py
@@ -7,10 +7,10 @@ from test_utils.test_utils import run_packager, is_tracked, does_app_support_ima
 
 def test_01_crete_sysroot(test_image, packager_binary, context, test_repo, test_sysroot):
     """TODO"""
-    package = "test_package"
+    package = "test_package_1"
     app = "io-module"
 
-    prepare_packages([package], context)
+    prepare_packages([package])
     # print(does_app_support_image(app, test_image))
 
     run_packager(
@@ -22,6 +22,7 @@ def test_01_crete_sysroot(test_image, packager_binary, context, test_repo, test_
         name=package,
         expected_result=True,
     )
+    assert is_tracked(package, test_repo, "package")
 
     run_packager(
         packager_binary,
@@ -39,13 +40,119 @@ def test_01_crete_sysroot(test_image, packager_binary, context, test_repo, test_
         "create-sysroot",
         context=context,
         image_name=test_image,
+        sysroot_dir=test_sysroot,
+        git_lfs=test_repo,
+        expected_result=True,
+    )
+    print("done")
+    # assert is_tracked(app, test_repo, "app")
+
+
+def test_02_create_sysroot_inconsistent_image_names(packager_binary, context, test_repo, test_sysroot):
+    """TODO"""
+    package = "test_package_1"
+    app = "io-module"
+
+    prepare_packages([package])
+    # print(does_app_support_image(app, test_image))
+
+    run_packager(
+        packager_binary,
+        "build-package",
+        context=context,
+        image_name="fedora41",
+        output_dir=test_repo,
+        name=package,
+        expected_result=True,
+    )
+    assert is_tracked(package, test_repo, "package")
+
+    run_packager(
+        packager_binary,
+        "build-package",
+        context=context,
+        image_name="fedora40",
+        output_dir=test_repo,
+        name=package,
+        expected_result=True,
+    )
+    assert is_tracked(package, test_repo, "package")
+
+    run_packager(
+        packager_binary,
+        "build-app",
+        context=context,
+        image_name="fedora40",
         output_dir=test_repo,
         name=app,
         expected_result=True,
     )
-    # assert is_tracked(app, test_repo, "app")
+    assert is_tracked(app, test_repo, "app")
+
+    run_packager(
+        packager_binary,
+        "create-sysroot",
+        context=context,
+        image_name="fedora41",
+        sysroot_dir=test_sysroot,
+        git_lfs=test_repo,
+        expected_result=True,
+    )
+    print("done")
 
 
-def test_02_create_sysroot_inconsistent_image_names(test_image, packager_binary, context, test_repo, test_sysroot):
+def test_03_create_sysroot_from_empty_repo(packager_binary, context, test_repo, test_sysroot):
     """TODO"""
-    return
+    run_packager(
+        packager_binary,
+        "create-sysroot",
+        context=context,
+        image_name="fedora41",
+        sysroot_dir=test_sysroot,
+        git_lfs=test_repo,
+        expected_result=False,
+    )
+    print("done")
+
+
+def test_04_create_sysroot_from_repo_with_packages_for_different_images(
+    packager_binary, context, test_repo, test_sysroot
+):
+    """TODO"""
+    package = "test_package_1"
+
+    prepare_packages([package])
+    # print(does_app_support_image(app, test_image))
+
+    run_packager(
+        packager_binary,
+        "build-package",
+        context=context,
+        image_name="fedora41",
+        output_dir=test_repo,
+        name=package,
+        expected_result=True,
+    )
+    assert is_tracked(package, test_repo, "package")
+
+    run_packager(
+        packager_binary,
+        "build-package",
+        context=context,
+        image_name="fedora40",
+        output_dir=test_repo,
+        name=package,
+        expected_result=True,
+    )
+    assert is_tracked(package, test_repo, "package")
+
+    run_packager(
+        packager_binary,
+        "create-sysroot",
+        context=context,
+        image_name="ubuntu2204",
+        sysroot_dir=test_sysroot,
+        git_lfs=test_repo,
+        expected_result=False,
+    )
+    print("done")
