4fea16ea2a2fa70f0931a7e5b5e8d944467ed963
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-03-26 10:36:01 +0100

    Add build package tests

--- tests/integration_tests/test_build_apps.py
+++ tests/integration_tests/test_build_apps.py
@@ -2,12 +2,21 @@ import subprocess
 import os
 from time import sleep
 
-from test_utils.test_utils import run_packager, does_image_exist, check_stdout, is_package_tracked, prepare_packages
+from test_utils.test_utils import (
+    run_packager,
+    does_image_exist,
+    check_stdout,
+    is_tracked,
+    prepare_packages,
+    does_app_support_image,
+)
 
 
 def test_01_build_app(test_image, packager_binary, context, test_repo):
     """TODO"""
     app = "io-module"
+    # app = "mission-module"
+    print(does_app_support_image(app, test_image))
 
     run_packager(
         packager_binary,
@@ -16,13 +25,39 @@ def test_01_build_app(test_image, packager_binary, context, test_repo):
         image_name=test_image,
         output_dir=test_repo,
         name=app,
+        expected_result=True,
     )
+    assert is_tracked(app, test_repo, "app")
 
 
-def test_02_build_all_apps(test_image, packager_binary, context, test_repo):
+def test_02_build_multiple_apps(test_image, packager_binary, context, test_repo):
     """TODO"""
-    apps = ["io-module", "io-module2"]
+    apps = ["io-module", "mission-module"]
+    run_packager(
+        packager_binary,
+        "build-app",
+        context=context,
+        image_name=test_image,
+        output_dir=test_repo,
+        name=apps[0],
+        expected_result=True,
+    )
+    run_packager(
+        packager_binary,
+        "build-app",
+        context=context,
+        image_name=test_image,
+        output_dir=test_repo,
+        name=apps[1],
+        expected_result=True,
+    )
+    for app in apps:
+        assert is_tracked(app, test_repo, "app")
 
+
+def test_03_build_all_apps(test_image, packager_binary, context, test_repo):
+    """TODO"""
+    apps = ["io-module", "mission-module"]
     run_packager(
         packager_binary,
         "build-app",
@@ -31,3 +66,5 @@ def test_02_build_all_apps(test_image, packager_binary, context, test_repo):
         output_dir=test_repo,
         all=True,
     )
+    for app in apps:
+        assert is_tracked(app, test_repo, "app")
