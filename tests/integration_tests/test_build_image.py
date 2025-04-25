e4f7b46fcb55227eb5c6184f38c9ac440281aef8
Ondrej Vala <93932164+ondrejvala2@users.noreply.github.com>
2025-04-25 13:20:41 +0200

    Add build all images test

--- tests/integration_tests/test_build_image.py
+++ tests/integration_tests/test_build_image.py
@@ -1,9 +1,6 @@
-import subprocess
-import os
-from time import sleep
-
-from test_utils.test_utils import run_packager, does_image_exist, check_stdout
 import pytest
+
+from test_utils.test_utils import run_packager, does_image_exist, check_stdout, test_config
 
 
 def test_01_show_help(packager_binary):
@@ -23,3 +20,14 @@ def test_02_build_image(test_image, packager_binary, context):
 
     run_packager(packager_binary, "build-image", context=context, image_name=test_image, expected_result=True)
     assert does_image_exist(test_image)
+
+
+def test_03_build_all_images(packager_binary, context, request):
+    """TODO"""
+    if request.config.getoption("--image") != "all":
+        pytest.skip("Skipping test because --image is not set to 'all'")
+
+    run_packager(packager_binary, "build-image", context=context, all=True, expected_result=True)
+
+    for image in test_config["test_images"]:
+        assert does_image_exist(image)
