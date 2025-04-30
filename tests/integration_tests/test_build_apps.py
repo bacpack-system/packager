805f9806dff07f71d07be2428343e62e746d51a8
Ondrej Vala <93932164+ondrejvala2@users.noreply.github.com>
2025-04-30 11:24:06 +0200

    Update test_build_apps.py

--- tests/integration_tests/test_build_apps.py
+++ tests/integration_tests/test_build_apps.py
@@ -13,8 +13,6 @@ from test_utils.test_utils import (
 def test_01_build_app(test_image, packager_binary, context, test_repo):
     """TODO"""
     app = "io-module"
-    # app = "mission-module"
-    print(does_app_support_image(app, test_image))
 
     run_packager(
         packager_binary,
@@ -23,9 +21,12 @@ def test_01_build_app(test_image, packager_binary, context, test_repo):
         image_name=test_image,
         output_dir=test_repo,
         name=app,
-        expected_result=True,
+        expected_result=None if not does_app_support_image(app, test_image) else True,
     )
-    assert is_tracked(app, test_repo, "app")
+    if does_app_support_image(app, test_image):
+        assert is_tracked(app, test_repo, "app")
+    else:
+        assert not is_tracked(app, test_repo, "app")
 
 
 def test_02_build_multiple_apps(test_image, packager_binary, context, test_repo):
@@ -38,7 +39,7 @@ def test_02_build_multiple_apps(test_image, packager_binary, context, test_repo)
         image_name=test_image,
         output_dir=test_repo,
         name=apps[0],
-        expected_result=True,
+        expected_result=None if not does_app_support_image(apps[0], test_image) else True,
     )
 
     clean_sysroot()
@@ -50,10 +51,13 @@ def test_02_build_multiple_apps(test_image, packager_binary, context, test_repo)
         image_name=test_image,
         output_dir=test_repo,
         name=apps[1],
-        expected_result=True,
+        expected_result=None if not does_app_support_image(apps[1], test_image) else True,
     )
     for app in apps:
-        assert is_tracked(app, test_repo, "app")
+        if does_app_support_image(app, test_image):
+            assert is_tracked(app, test_repo, "app")
+        else:
+            assert not is_tracked(app, test_repo, "app")
 
 
 def test_03_build_all_apps(test_image, packager_binary, context, test_repo):
@@ -66,6 +70,10 @@ def test_03_build_all_apps(test_image, packager_binary, context, test_repo):
         image_name=test_image,
         output_dir=test_repo,
         all=True,
+        expected_result=None,
     )
     for app in apps:
-        assert is_tracked(app, test_repo, "app")
+        if does_app_support_image(app, test_image):
+            assert is_tracked(app, test_repo, "app")
+        else:
+            assert not is_tracked(app, test_repo, "app")
