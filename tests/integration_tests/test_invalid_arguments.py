8147043d6a1a0d078f4bb7cd9732025c80a2b438
Ondrej Vala <93932164+ondrejvala2@users.noreply.github.com>
2025-06-23 14:00:08 +0200

    Update test_invalid_arguments.py

--- tests/integration_tests/test_invalid_arguments.py
+++ tests/integration_tests/test_invalid_arguments.py
@@ -4,10 +4,12 @@ import os
 
 from test_utils.test_utils import run_packager
 
+from test_utils.common import PackagerReturnCode
+
 
 def test_01_run_without_command(packager_binary):
     """TODO"""
-    result = run_packager(packager_binary, "")
+    result = run_packager(packager_binary, "", expected_returncode=PackagerReturnCode.CMD_LINE_ERROR)
     stdout = result.communicate()[0]
 
     assert "ERROR" in stdout
@@ -15,7 +17,7 @@ def test_01_run_without_command(packager_binary):
 
 def test_02_run_with_invalid_command(packager_binary):
     """TODO"""
-    result = run_packager(packager_binary, "invalid-command")
+    result = run_packager(packager_binary, "invalid-command", expected_returncode=PackagerReturnCode.CMD_LINE_ERROR)
     stdout = result.communicate()[0]
 
     assert "ERROR" in stdout
@@ -25,7 +27,13 @@ def test_03_run_with_nonexisting_image(packager_binary):
     """TODO"""
     context = os.path.abspath(os.path.join("test_data", "example"))
 
-    result = run_packager(packager_binary, "build-image", context=context, image_name="nonexisting_image")
+    result = run_packager(
+        packager_binary,
+        "build-image",
+        context=context,
+        image_name="nonexisting_image",
+        expected_returncode=PackagerReturnCode.DEFAULT_ERROR,
+    )
     stdout = result.communicate()[0]
 
     assert "ERROR" in stdout
@@ -43,6 +51,7 @@ def test_04_run_with_nonexisting_package(test_image, packager_binary, test_repo)
         image_name=test_image,
         name="nonexisting_package",
         output_dir=test_repo,
+        expected_returncode=PackagerReturnCode.DEFAULT_ERROR,
     )
     stdout = result.communicate()[0]
 
@@ -60,6 +69,7 @@ def test_05_run_with_nonexisting_app(test_image, packager_binary, test_repo):
         image_name=test_image,
         name="nonexisting_app",
         output_dir=test_repo,
+        expected_returncode=PackagerReturnCode.DEFAULT_ERROR,
     )
     stdout = result.communicate()[0]
 
